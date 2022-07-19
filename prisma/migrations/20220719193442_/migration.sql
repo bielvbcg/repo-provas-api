15f6e3fd376f0c389d514cd375d5702003842dd5
bielvbcg <gabrielvbcallegario@gmail.com>
2022-07-19 16:39:13 -0300

    add seed.ts file for prisma

---
+++ prisma/migrations/20220719193442_/migration.sql
@@ -0,0 +1,14 @@
+-- DropIndex
+DROP INDEX "disciplines_termId_key";
+
+-- DropIndex
+DROP INDEX "teachersDisciplines_disciplineId_key";
+
+-- DropIndex
+DROP INDEX "teachersDisciplines_teacherId_key";
+
+-- DropIndex
+DROP INDEX "tests_categoryId_key";
+
+-- DropIndex
+DROP INDEX "tests_teacherDisciplineId_key";
---
+++ prisma/seed.ts
@@ -0,0 +1,99 @@
+import { prisma } from "../src/database.js"
+
+async function main() {
+  const terms = []
+  const categories = []
+  const teachers = []
+  const disciplines = []
+  const teachersDisciplines = []
+
+  const termsList = [1, 2, 3, 4, 5, 6]
+  const categoriesList = ["Projeto", "Prática", "Recuperação"]
+  const teachersList = ["Diego Pinho", "Bruna Hamori"]
+  const disciplinesList = [
+    { name: "HTML e CSS", termId: 1 },
+    { name: "JavaScript", termId: 2 },
+    { name: "React", termId: 3 },
+    { name: "Humildade", termId: 1 },
+    { name: "Planejamento", termId: 2 },
+    { name: "Autoconfiança", termId: 3 },
+  ]
+  const teachersDisciplinesList = [
+    { teacherId: 1, disciplineId: 1 },
+    { teacherId: 1, disciplineId: 2 },
+    { teacherId: 1, disciplineId: 3 },
+    { teacherId: 2, disciplineId: 1 },
+    { teacherId: 2, disciplineId: 2 },
+    { teacherId: 2, disciplineId: 3 },
+  ]
+
+  for (let i = 0; i < termsList.length; i++) {
+    terms[i] = await prisma.term.upsert({
+      where: { number: termsList[i] },
+      update: {},
+      create: { number: termsList[i] },
+    })
+  }
+
+  for (let i = 0; i < categoriesList.length; i++) {
+    categories[i] = await prisma.category.upsert({
+      where: { name: categoriesList[i] },
+      update: {},
+      create: { name: categoriesList[i] }
+    })
+  }
+
+  for (let i = 0; i < teachersList.length; i++) {
+    teachers[i] = await prisma.teacher.upsert({
+      where: { name: teachersList[i] },
+      update: {},
+      create: { name: teachersList[i] }
+    })
+  }
+
+  for (let i = 0; i < disciplinesList.length; i++) {
+    disciplines[i] = await prisma.discipline.upsert({
+      where: { name: disciplinesList[i].name },
+      update: {},
+      create: {
+        name: disciplinesList[i].name,
+        termId: disciplinesList[i].termId
+      }
+    })
+  }
+
+  for (let i = 0; i < teachersDisciplinesList.length; i++) {
+    teachersDisciplines[i] = await prisma.teachersDisciplines.upsert({
+      where: { id: i + 1 },
+      update: {},
+      create: {
+        teacherId: teachersDisciplinesList[i].teacherId,
+        disciplineId: teachersDisciplinesList[i].disciplineId
+      }
+    })
+  }
+
+  console.log(
+    terms,
+    categories,
+    teachers,
+    disciplines,
+    teachersDisciplines
+  )
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
+
+/* INSERT INTO terms("number") VALUES(1);
+INSERT INTO terms("number") VALUES(2);
+INSERT INTO terms("number") VALUES(3);
+INSERT INTO terms("number") VALUES(4);
+INSERT INTO terms("number") VALUES(5);
+INSERT INTO terms("number") VALUES(6); */
